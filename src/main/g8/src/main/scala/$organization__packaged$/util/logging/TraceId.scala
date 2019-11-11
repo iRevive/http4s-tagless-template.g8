6ba47feb84024bd866d78ab9ea2ecf669f232a87
Maksim Ochenashko <maxochenashko@gmail.com>
2019-11-11 17:01:25 +0200

    Refactor TraceId

--- src/main/g8/src/main/scala/$organization__packaged$/util/logging/TraceId.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/util/logging/TraceId.scala
@@ -5,31 +5,21 @@ import java.util.UUID
 import cats.Functor
 import cats.effect.Sync
 import cats.syntax.functor._
-import $organization$.util.logging.TraceId._
 import com.typesafe.scalalogging.CanLog
 
 import scala.util.Random
 
 sealed trait TraceId {
-  def child(traceId: TraceId): TraceId = TraceIdPath(this, traceId)
+  def child(traceId: TraceId): TraceId = TraceId./(this, traceId)
 }
 
 object TraceId {
 
-  object / {
-    def unapply(arg: TraceId): Option[(TraceId, TraceId)] =
-      arg match {
-        case TraceIdPath(p, c) => Some((p, c))
-        case _                 => None
-      }
-  }
-
-  private[TraceId] final case class TraceIdPath(parent: TraceId, child: TraceId) extends TraceId
-
-  final case class ApiRoute(value: String)     extends TraceId
-  final case class Const(value: String)        extends TraceId
-  final case class Alphanumeric(value: String) extends TraceId
-  final case class Uuid(value: UUID)           extends TraceId
+  final case class /(parent: TraceId, child: TraceId) extends TraceId
+  final case class ApiRoute(value: String)            extends TraceId
+  final case class Const(value: String)               extends TraceId
+  final case class Alphanumeric(value: String)        extends TraceId
+  final case class Uuid(value: UUID)                  extends TraceId
 
   def randomUuid[F[_]: Sync]: F[Uuid] =
     Sync[F].delay(Uuid(UUID.randomUUID()))
@@ -48,7 +38,7 @@ object TraceId {
     @SuppressWarnings(Array("org.wartremover.warts.Recursion"))
     def loop(elem: TraceId, out: List[String]): List[String] =
       elem match {
-        case TraceIdPath(h, t)   => loop(t, loop(h, out))
+        case h / t               => loop(t, loop(h, out))
         case ApiRoute(value)     => out :+ value
         case Const(value)        => out :+ value
         case Alphanumeric(value) => out :+ value
@@ -59,7 +49,7 @@ object TraceId {
   }
 
   implicit object CanLogTraceId extends CanLog[TraceId] {
-    override def logMessage(originalMsg: String, a: TraceId): String = s"[\${TraceId.render(a, "-")}] \$originalMsg"
+    override def logMessage(originalMsg: String, a: TraceId): String = s"[\${TraceId.render(a, "#")}] \$originalMsg"
   }
 
 }
