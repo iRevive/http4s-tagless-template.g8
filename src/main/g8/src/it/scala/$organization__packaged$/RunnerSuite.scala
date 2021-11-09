1ba9336381b96f23196ab72c8c10b395b7b05911
Maksim Ochenashko <maxochenashko@gmail.com>
2021-11-09 11:39:12 +0200

    disable runner suite

--- src/main/g8/src/it/scala/$organization__packaged$/RunnerSuite.scala
+++ src/main/g8/src/it/scala/$organization__packaged$/RunnerSuite.scala
@@ -1,18 +1,18 @@
 package $organization$
 
-import cats.data.{EitherT, Kleisli}
+/*import cats.data.{EitherT, Kleisli}
 import cats.effect.{Async, ExitCode, IO}
 import cats.syntax.either.*
 import $organization$.it.{AppSuite, EffUnsafeRun}
 import $organization$.persistence.postgres.PostgresError
 import $organization$.util.error.{AppError, RaisedError}
 import $organization$.util.execution.Eff
-import io.odin.meta.Position
+import io.odin.meta.Position*/
 import weaver.SimpleMutableIOSuite
 
 object RunnerSuite extends SimpleMutableIOSuite {
 
-  test("execute a job via main method") {
+  /*test("execute a job via main method") {
     val runner = new Runner.Simple {
       override def name: String = "test"
 
@@ -70,6 +70,6 @@ object RunnerSuite extends SimpleMutableIOSuite {
     for {
       r <- runner.run(Nil).start.flatMap(f => f.cancel).attempt
     } yield expect(r.isRight)
-  }
+  }*/
 
 }
